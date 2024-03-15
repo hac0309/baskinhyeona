bd36a079ab41d102ec2f28b74563e0f7ddf82a6a
choi_hyeona <hac0309@naver.com>
2024-03-15 18:30:11 +0900

    240315 집가서 다시 해보기...흑..흑ㅎ.ㄱ

--- JS/mian.js
+++ JS/mian.js
@@ -31,3 +31,11 @@ new Swiper('.new-item .swiper', {
     clickable: true 
   }
 });
+
+new Swiper('.br-benefit .swiper', {
+  loop: false, // 반복 재생 여부, 1->2->3->4->1->2...
+  autoplay: true, // 자동 재생 여부
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수 (기본값: 1)
+  spaceBetween: 30, // 슬라이드 사이 여백(간격)px
+  centeredSlides: false // 1번 슬라이드가 가운데 보이기
+});
