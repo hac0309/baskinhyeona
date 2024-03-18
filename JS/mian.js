50eae0dfce95775e59577afd0be21afaa450cc0a
choi_hyeona <hac0309@naver.com>
2024-03-18 11:42:09 +0900

    240318 헤더..어음...햤다,,

--- JS/mian.js
+++ JS/mian.js
@@ -1,3 +1,13 @@
+const headerEl = document.querySelector('header');
+window.addEventListener('scroll', function () {
+  // console.log(window.scrollY);
+  if (window.scrollY > 10) {
+    headerEl.classList.add('scroll_on');
+  } else {
+    headerEl.classList.remove('scroll_on');
+  }
+});
+
 const btnCloseEl = document.querySelector('.btn-close');
 const visualProEl = document.querySelector('.visual-promotion');
 
@@ -40,15 +50,13 @@ new Swiper('.br-benefit .swiper', {
   centeredSlides: false 
 });
 
-// const headerEl = document.querySelector('header')
-
-// if (scrollY > 10) {
-//   headerEl.style.
-// } else {
-  
-// }
-
-
 new Date().getFullYear(); 
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();
+
+const loginSebEl = document.querySelector('.login_seb');
+const personBtn = document.querySelector('.person-btn');
+
+personBtn.addEventListener('click', function () {
+  loginSebEl.classList.toggle('active');
+})
